1340d3e0bb751f810d7bacacc963153529ddc4f0
Maksim Stankevic <maksim.stankevic1@gmail.com>
2019-01-31 14:18:09 +0200

    addingPyramidTxt

--- src/local/pyramid/Main.java
+++ src/local/pyramid/Main.java
@@ -1,8 +1,16 @@
 package local.pyramid;
 
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        Stream<Integer> s = Stream.of(1);
+        IntStream is = s.mapToInt(x -> x);
+        DoubleStream ds = s.mapToDouble(x -> x);
+        Stream<Double> s2 = ds.mapToObj(x -> x);
+        s2.forEach(System.out::print);
+	}
 }
