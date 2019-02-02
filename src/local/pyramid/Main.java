628a0c6ad9f35ae223307276f69e86f18e2781ec
Maksim Stankevic <maksim.stankevic1@gmail.com>
2019-02-02 16:59:33 +0200

    reallyFinishedJava

--- src/local/pyramid/Main.java
+++ src/local/pyramid/Main.java
@@ -21,120 +21,57 @@ public class Main {
             System.out.println("Could not read " + path.toString());
         }
 
+        System.out.println("Contents of pyramid.txt:\n");
         for (String [] arr: parsedData) System.out.println(Arrays.toString(arr));
 
-        List<String> fullPaths = new ArrayList<>();
-        Set<String> oldPaths = new HashSet<>();
-        int counter = 0;
-
-
-        Map<String,Integer> badCounts = new HashMap<>();
-
+        Set<String> nicelyFormattedFullPaths = new HashSet<>();
+        Set<String> usedAndDeadPaths = new HashSet<>();
 
         boolean reachedTheEND = false;
 
         while(!reachedTheEND) {
 
-            String [] temp = new String[parsedData.size()];
-
+            String [] buffer = new String[parsedData.size()];
 
             for (int i = 0, parent = 0; i < parsedData.size(); i++) {
                 if (i == 0) {
-                    temp[i] = (parsedData.get(i))[i];
+                    buffer[i] = (parsedData.get(i))[i];
                 } else {
+
                     String[] children = new String[] {(parsedData.get(i))[parent], (parsedData.get(i))[parent+1]};
-                    boolean needOddNow = Integer.parseInt(temp[i - 1]) % 2 == 0;
+                    boolean needOddNow = Integer.parseInt(buffer[i - 1]) % 2 == 0;
                     int oneOrZero = needOddNow ? 1 : 0;
+                    String pathSoFar = getPathWithInt(i, buffer);
 
-
-                    String pathSoFar = getPathWithInt(i, temp);
-
-                    if (Integer.parseInt(children[0])%2 == oneOrZero && !oldPaths.contains(pathSoFar)) {
-                        temp[i] = children[0];
+                    if (Integer.parseInt(children[0])%2 == oneOrZero && !usedAndDeadPaths.contains(pathSoFar)) {
+                        buffer[i] = children[0];
                     } else if (Integer.parseInt(children[1])%2 == oneOrZero) {
-                        temp[i] = children[1];
+                        buffer[i] = children[1];
                         parent++;
                     } else {
-
-                        if (!oldPaths.add(pathSoFar)) {
+                        if (!usedAndDeadPaths.add(pathSoFar)) {
                             int repeatCount = 1;
-                            while (i - repeatCount > 0 && !oldPaths.add(getPathWithInt(i - repeatCount, temp))) repeatCount++;
+                            while (i - repeatCount > 0 && !usedAndDeadPaths.add(getPathWithInt(i - repeatCount, buffer))) repeatCount++;
                             if (i - repeatCount == 0) reachedTheEND = true;
 
                         }
-
                         break;
                     }
 
-                }
-            }
+                    if (i == parsedData.size() - 1) {
 
+                        String goodPath = getPathWithIntSpaceDelimited(parsedData.size(), buffer);
+                        nicelyFormattedFullPaths.add(goodPath.trim());
+                        String badPath = getPathWithInt(parsedData.size(), buffer);
+                        int repeatCount = 0;
+                        while (!usedAndDeadPaths.add(getPathWithInt(i - repeatCount, buffer))) repeatCount++;
 
-
-
-            String goodPath = getPathWithIntSpaceDelimited(parsedData.size(), temp);
-
-
-            if (temp[parsedData.size() - 1] != null) {
-                if (!fullPaths.contains(goodPath.trim())) {
-
-                    fullPaths.add(goodPath.trim());
-                    String badPath = getPathWithInt(parsedData.size(), temp);
-                    badCounts.put(badPath, 1);
-                    oldPaths.add(badPath);
-
-                } else {
-
-                    String usedFullPath = getPathWithInt(parsedData.size(), temp);
-                    String veryBadPath = getPathWithInt(parsedData.size() - badCounts.get(usedFullPath), temp);
-                    badCounts.put(usedFullPath, badCounts.get(usedFullPath) + 1);
-                    while (!oldPaths.add(veryBadPath)) {
-                        veryBadPath = getPathWithInt(parsedData.size() - badCounts.get(usedFullPath), temp);
-                        badCounts.put(usedFullPath, badCounts.get(usedFullPath) + 1);
                     }
-
-
                 }
-
             }
-
-
-//            System.out.println("INSIDE WHILE, counter - " + counter);
-//            System.out.println("VARIABLE fullPaths " + fullPaths);
-//            System.out.println("VARIABLE oldPaths " + oldPaths);
-//            System.out.println("VARIABLE badCounts" + badCounts);
-
-
-
-            counter++;
         }
 
-
-//        System.out.println("VARIABLE oldPaths " + oldPaths);
-//        System.out.println("VARIABLE oldPaths SIZE " + oldPaths.size());
-//        oldPaths.stream().sorted().forEach(System.out::println);
-//        System.out.println("VARIABLE badCounts" + badCounts);
-
-
-        System.out.println("\nNumber of valid paths found: " + fullPaths.size() + "\n");
-        long max = 0;
-        String winner = "";
-        for (String entry: fullPaths){
-            String [] array = entry.split(" ");
-            System.out.print(Arrays.toString(array) + "\t\t\t");
-            long sum = 0;
-            for (int k = 0; k < parsedData.size(); k++) {
-                sum += Long.parseLong(array[k]);
-                winner = (sum > max) ? Arrays.toString(array) : winner;
-                max = (sum > max) ? sum : max;
-
-            }
-            System.out.println("Sum of this path: " + sum);
-
-        }
-        System.out.println("\n\n\nMaximum is " + max + " and it corresponds to the path: " + winner);
-
-
+        outputResults(nicelyFormattedFullPaths, parsedData.size());
 
 
     }
@@ -150,8 +87,37 @@ public class Main {
     private static String getPathWithIntSpaceDelimited (int depth, String [] stringArray) {
         StringBuilder path = new StringBuilder();
         for (int j = 0; j < depth; j++) {
-            path.append(stringArray[j] + " ");
+            path.append(stringArray[j]);
+            path.append(" ");
         }
         return path.toString();
+    }
+
+    private static void outputResults(Set<String> paths, int nrRows){
+
+        System.out.println("\nNumber of valid paths found: " + paths.size() + "\n");
+
+        int max = 0;
+        String winner = "";
+
+        for (String entry: paths){
+
+            int sum = 0;
+            String [] array = entry.split(" ");
+            System.out.print(Arrays.toString(array) + "\t\t\t");
+
+            for (int k = 0; k < nrRows; k++) {
+                sum += Integer.parseInt(array[k]);
+                winner = (sum > max) ? Arrays.toString(array) : winner;
+                max = (sum > max) ? sum : max;
+
+            }
+
+            System.out.println("Sum of this path: " + sum);
+
+        }
+
+        System.out.println("\n\n\nMaximum is " + max + " and it corresponds to the path: " + winner);
+
     }
 }
